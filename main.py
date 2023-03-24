deda9c0dde04dc82473bef0b58b9553c70385f4c
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2023-03-24 15:42:12 -0300

    update

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 class domino_mexicano:
     def __init__(self):
+        self.carrocao = []
+        self.pontuacao_jogador = []
         self.numero_participantes = int(input("Digite o número de jogadores: "))
         self.n=0
         self.lista = []
         while self.n <= self.numero_participantes-1:
             nome_participante = input("Digite o nome do jogador: ")
-            self.lista.append({nome_participante})
+            self.lista.append(nome_participante)
             self.n += 1
         self.lista.reverse()
-### INVERTER A LISTA
 
     def pontuacao_carrocao(self):
-        numero_carrocao = int(input("Digite numero do carrocao: "))
+        while len(self.carrocao) <= 1:
+            numero_carrocao = int(input("Digite numero do carrocao: "))
+            self.carrocao.append(numero_carrocao)
 
-        while self.n >= 1:
-            pontuacao_jogador = input(f"Digite a pontuação de {self.lista[self.n-1]}: ")
-            #self.lista.append({self.lista[self.n-1]:pontuacao_jogador[self.n-1]})
-            self.n -= 1
-        #print(self.lista)
-            print(self.lista[self.n])
+            while self.n >= 1:
+                pontos = input(f"Digite a pontuação de {self.lista[self.n-1]}: ")
+                self.pontuacao_jogador.append({self.lista[self.n-1]:pontos})
+                self.n -= 1
+            self.n=2
+
+
+
+        #print(self.pontuacao_jogador)
+
 
 
 jogatina = domino_mexicano()
