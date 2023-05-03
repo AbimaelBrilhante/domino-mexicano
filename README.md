f60fd6ed251fd783fe71dc836c8f2c6ea0bb6fb1
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2023-05-02 21:38:43 -0300

    update_melhorias

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # domino-mexicano
 
 em desenvolvimento
+>fazer validação do numero de carroções
+>não permitir repetição de carroção
+>verificar qual forma será o output dos dados
+>transcrever o codigo para javascript com HTML5 e CSS3
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class domino_mexicano:
+
+## gera as variaveis com a quantidade de jogadores e nome dos jogadores
     def __init__(self):
         self.carrocao = []
         self.pontuacao_jogador = []
@@ -10,11 +12,15 @@ class domino_mexicano:
             self.lista.append(nome_participante)
             self.n += 1
         self.lista.reverse()
+        
+#faz um loop com a quantidade de carroção do dominó (fazer validação para 13 e não permitir repetidos)
 
     def pontuacao_carrocao(self):
         while len(self.carrocao) <= 1:
             numero_carrocao = int(input("Digite numero do carrocao: "))
             self.carrocao.append(numero_carrocao)
+
+            #traz o resultado como dict dentreo de uma lista
 
             while self.n >= 1:
                 pontos = input(f"Digite a pontuação de {self.lista[self.n-1]}: ")
