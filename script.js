01d4fa19224b959c90c3f5adc7ce887d9ed86f88
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2023-05-10 12:19:25 -0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-var el = carrocao
-function Mudarestado(el) {
-    document.getElementById(el).style.display = 'block';
-}
+document.getElementById('carrocao').style.display = "none";
 
 var carrocao = [[],[],[],[],[],[]] 
 var numero_participantes_original = 2 //alterar para input do HTML
@@ -10,7 +6,7 @@ var numero_participantes = numero_participantes_original
 var  n = 0  
 var lista = [] 
 while (n <= numero_participantes_original-1){ 
-            var nome_participante = prompt("Digite o nome do Participante: ");
+            //var nome_participante = prompt("Digite o nome do Participante: ");
             lista.push(nome_participante)
             n += 1
         }    
@@ -19,12 +15,12 @@ while (n <= numero_participantes_original-1){
 function pontuacao_carrocao(){
     var qtd_carrocao = 2 //quanidade de carrocoes do jogo = 13
     while (qtd_carrocao >= 1){ 
-    numero_carrocao = prompt("Digite o numero do Carroção: ")
+    //numero_carrocao = prompt("Digite o numero do Carroção: ")
     Number(numero_carrocao)
     
 
         while (numero_participantes_original >= 1){ 
-            pontos = prompt(`'Digite a pontuação de ${lista[n-1]}: `) 
+            //pontos = prompt(`'Digite a pontuação de ${lista[n-1]}: `) 
             carrocao[numero_carrocao].push(pontos) 
             numero_participantes_original-=1 
             n-=1 
