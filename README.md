37a0796b20d73d5f3507892ca1eeca87329c31c4
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2023-05-11 16:18:55 -0300

    update

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # domino-mexicano
 
 em desenvolvimento
->os dados do ultimo input estão invertidos, porem o output está correto
->não permitir repetição de carroção
->verificar qual forma será o output dos dados
->transcrever o codigo para javascript com HTML5 e CSS3
->fazer o try/except para validação do input
--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//document.getElementById('carrocao').style.display = "block";
-
-/*var carrocao = [[],[],[],[],[],[]] 
-*/
-n = 0 
 lista = []
 function nome_participante(){
     nome_participante = (document.getElementById('nome_participante'))
@@ -13,13 +8,13 @@ function nome_participante(){
     console.log(numero_participantes)
 }
 
-
 function tela_cadastro(){
     document.getElementById('cadastrar_while').onclick = nome_participante;
 } 
 
 function submit_1(){
     document.getElementById('cadastrar').onclick = none_primeira_tela;
+    numero_carrocao()
 }
 
 function none_primeira_tela(){
@@ -29,30 +24,38 @@ function none_primeira_tela(){
 
 function numero_carrocao(){
     qtd_carrocao = 13 //quanidade de carrocoes do jogo = 13
-    numero_carrocao = (document.getElementById('numero_carrocao'))
-
+    numero_carrocao_digitado = (document.getElementById('numero_carrocao'))
+    document.getElementById('submit_numero_carrocao').onclick = pontuacao_carrocao
+    console.log(numero_carrocao_digitado.value)
 }
 
+carrocao = [[],[],[],[],[],[]] 
 function pontuacao_carrocao(){
-    var qtd_carrocao = 13 //quanidade de carrocoes do jogo = 13
+    document.getElementById('jogatina').style.display = "block";
+    document.getElementById('carrocao').style.display = "none"  
+    var qtd_carrocao = 2 //quanidade de carrocoes do jogo = 13
     while (qtd_carrocao >= 1){ 
-    //numero_carrocao = prompt("Digite o numero do Carroção: ")
-    Number(numero_carrocao)
+    Number(numero_carrocao_digitado.value)
+    console.log(numero_carrocao_digitado)
     
 
         while (numero_participantes_original >= 1){ 
-            //pontos = prompt(`'Digite a pontuação de ${lista[n-1]}: `) 
-            carrocao[numero_carrocao].push(pontos) 
+            pontos = document.getElementById('pontuacao_carrocao_jogador')
+            document.getElementById('cadastrar_pontos_do_carrocao_jogador').onclick = pontuacao_jogador
             numero_participantes_original-=1 
-            n-=1 
-        }
 
-    n=0    
+        }
+  
     numero_participantes_original = numero_participantes   
-    qtd_carrocao -=1 
-        
+    qtd_carrocao -=1        
     }
 }  
+// EDITAR
+function pontuacao_jogador(){
+    console.log(carrocao[numero_carrocao_digitado])
+    carrocao[numero_carrocao_digitado.value].push(pontos.value)
+    console.log(carrocao)    
+}
 
 tela_cadastro()
 submit_1()
