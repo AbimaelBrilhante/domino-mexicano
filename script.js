c12395d321657e26bd6aafd180401aaf91c517e5
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2023-05-15 14:53:07 -0300

    update

--- script.js
+++ script.js
@@ -5,24 +5,18 @@ function nome_participante(){
     numero_participantes_original = lista.length //alterar para input do HTML
     numero_participantes = numero_participantes_original 
     console.log(lista)
-    console.log(numero_participantes)
+    document.getElementById('cadastrar').onclick = numero_carrocao;
+    //console.log(numero_participantes)
 }
 
 function tela_cadastro(){
     document.getElementById('cadastrar_while').onclick = nome_participante;
-    console.log(nome_participante.numero_participantes)
-    console.log(nome_participante.numero_participantes_original)
 } 
-
-function submit_1(){
-    document.getElementById('cadastrar').onclick = numero_carrocao;
-}
 
 function numero_carrocao(){
     document.getElementById('cadastro').style.display = "none";
     document.getElementById('carrocao').style.display = "block"   
-    document.getElementById('jogatina').style.display = "none"
-    
+    //document.getElementById('jogatina').style.display = "none"
     carrocao_repetido = []
     numero_carrocao_digitado = (document.getElementById('numero_carrocao'))
     carrocao_repetido.push(numero_carrocao_digitado.value) 
@@ -44,47 +38,31 @@ function numero_carrocao(){
 
 carrocao = [[],[],[],[],[],[]] 
 function pontuacao_carrocao(){
+    carrocao = [[],[],[],[],[],[]] 
 
     document.getElementById('jogatina').style.display = "block"
     document.getElementById('carrocao').style.display = "none" 
     console.log(numero_participantes_original) 
-    n=2
     while (numero_participantes_original>0){
         pontos = document.getElementById('pontuacao_carrocao_jogador')
-        carrocao[numero_carrocao_digitado.value].push(pontos.value)
+        //carrocao[numero_carrocao_digitado.value].push(pontos.value)
         document.getElementById('cadastrar_pontos_do_carrocao_jogador').onclick = pontuacao_jogador 
-
-    }
-    numero_carrocao    
-    //numero_participantes_original = nome_participante.numero_participantes
-
-
-
+        numero_participantes_original-=1
+        
+    }    
+    numero_carrocao()   
 }  
 // EDITAR - ESTÁ SENDO SOLICITADO UM DADO A MAIS
 function pontuacao_jogador(){
-    numero_participantes_original-=1   
-    pontuacao_carrocao  
+    if(numero_participantes>0){
+        carrocao[numero_carrocao_digitado.value].push(pontos.value)
+        console.log(carrocao)
+        numero_participantes-=1
+    }    
+    else{
+        numero_carrocao()
+    }
 }
 
+
 tela_cadastro()
-submit_1()
-
-
-
-/*function pontuacao_jogador(){
-    while(numero_participantes_original>0){
-        if(numero_participantes_original>0){
-            carrocao[numero_carrocao_digitado.value].push(pontos.value)
-            console.log(carrocao)
-            numero_participantes_original-=1 
-        }  
-            
-        else {
-            document.getElementById('jogatina').style.display = "none";
-            document.getElementById('carrocao').style.display = "block"  
-            numero_carrocao()
-            numero_participantes_original = numero_participantes 
-        } 
-    }          
-}*/
